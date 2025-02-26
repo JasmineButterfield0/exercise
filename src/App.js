efec91019a3140ef58aadb1fc89755b3d8fbf2a6
JasmineButterfield0 <ja895969@ucf.edu>
2025-02-25 22:04:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import RepetitionExercise from './components/RepetitionExercise';
 import Durationexercise from './components/DurationExercise';
-
+import DistanceExercise from './components/DistanceExercise';
 
 function App() {
   const [selectedExercise, setSelectedExercise] = useState(null);
@@ -13,12 +13,14 @@ function App() {
   return (
     <div className="App">
       <h1>Exercise Tracker</h1>
+      <button onClick={() => handleExerciseClick('distance')}>Walking</button>
+      <br></br>
       <button onClick={() => handleExerciseClick('repetition')}>Jumping Jacks</button>
       <br></br>
       <button onClick={() => handleExerciseClick('duration')}>Cycling</button>
-      <br></br>
 
 
+      {selectedExercise === 'distance' && <DistanceExercise name="Walking" />}
       {selectedExercise === 'repetition' && <RepetitionExercise name="Jumping Jacks" />}
       {selectedExercise === 'duration' && <Durationexercise name="Cycling" />}
     </div>
